a0564ce7110db619bb1106dfcd66b55acb0c4e3c
Kane Bold <koboll1985@gmail.com>
2021-09-01 14:01:25 +0500

    Start project.

---
+++ src/main/java/com/alfabank/kosmos/exchangerate/ExchangerateApplication.java
@@ -0,0 +1,13 @@
+package com.alfabank.kosmos.exchangerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangerateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alfabank/kosmos/exchangerate/ExchangerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfabank.kosmos.exchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangerateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
